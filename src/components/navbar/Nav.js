487b60f636f61ac0407aaccb9dcb94912be84e30
Aman <amankunda9@gmail.com>
2022-12-29 04:12:46 +0530

    ui changes

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
