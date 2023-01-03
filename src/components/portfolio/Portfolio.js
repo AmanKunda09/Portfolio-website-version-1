f9b3215c6a0f745c1c9f249ba13ef64f5d167b05
Aman <amankunda9@gmail.com>
2023-01-03 05:34:18 +0530

    added experiemnce and portfolio component

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,73 @@
 import React from "react";
+import "./Portfolio.css";
+import IMG from "../../assets/projectPagination.png";
+import IMG1 from "../../assets/portfolio1.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG,
+    title: "This is a portfolio item title",
+    github: "http://github.com",
+    demo: "http://github.com",
+  },
+  {
+    id: 2,
+    image: IMG,
+    title: "This is a portfolio item title",
+    github: "http://github.com",
+    demo: "http://github.com",
+  },
+  {
+    id: 3,
+    image: IMG,
+    title: "This is a portfolio item title",
+    github: "http://github.com",
+    demo: "http://github.com",
+  },
+  {
+    id: 4,
+    image: IMG,
+    title: "This is a portfolio item title",
+    github: "http://github.com",
+    demo: "http://github.com",
+  },
+  {
+    id: 5,
+    image: IMG,
+    title: "This is a portfolio item title",
+    github: "http://github.com",
+    demo: "http://github.com",
+  },
+];
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
